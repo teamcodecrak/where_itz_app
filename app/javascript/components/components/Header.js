9e7dd3bfb53cf0ddc0bbb317576ff440defbd953
kevans107 <kellyevans107.gmail.com>
2021-11-18 18:52:31 -0500

    header indentation cleaned up and json file removed

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -14,26 +14,26 @@ import logo  from '../assets/logo.png'
           
         return (
             <header>
-        <NavLink to="/">
-          <img src={logo} alt="where itz app logo" className="logo" width="100px" height="100px"/>
-        </NavLink>
-        <div className="nav-bar">
-        <ul>
-                {!loggedIn &&
-                <>
-                <a href={signInRoute} className="nav-link">Sign In</a>
-                <a href={newUserRoute} className="nav-link">Sign Up</a>
-                </>
-                }
-                {loggedIn &&
-                <>
-                <a href={signOutRoute} className="nav-link">Sign Out</a>
-                <NavLink to="/mylists" className="nav-link">My Lists</NavLink>
-                </>
-                }
-            </ul>
-        </div>
-      </header>
+                <NavLink to="/">
+                    <img src={logo} alt="where itz app logo" className="logo" width="100px" height="100px"/>
+                </NavLink>
+                <div className="nav-bar">
+                    <ul>
+                        {!loggedIn &&
+                        <>
+                            <a href={signInRoute} className="nav-link">Sign In</a>
+                            <a href={newUserRoute} className="nav-link">Sign Up</a>
+                        </>
+                        }
+                        {loggedIn &&
+                        <>
+                            <a href={signOutRoute} className="nav-link">Sign Out</a>
+                            <NavLink to="/mylists" className="nav-link">My Lists</NavLink>
+                        </>
+                        }
+                    </ul>
+                </div>
+            </header>
         )
     }
 }
