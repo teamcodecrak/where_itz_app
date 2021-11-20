8d54d67dd5ef4475be780db490b661f54281c0e6
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-11-20 06:49:51 -0800

    seeds.rb updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,20 +16,25 @@ movies = [
   year: 2019
   },
   {
-  title: '456 Street',
+  title: '5th Element',
   show_type: 'movie',
-  network: ['hulu']
-  genre: ['horror','foreign']
-  us_rating: 'R'
-  year: 2021
+  network: ['netflix']
+  genre: ['sci-fi','fantasy']
+  us_rating: 'PG-13'
+  year: 1997
   },
   {
-  title: '456 Street',
-  show_type: 'movie',
+  title: 'Jeffersons',
+  show_type: 'tv show',
   network: ['hulu']
-  genre: ['horror','foreign']
-  us_rating: 'R'
-  year: 2021
+  genre: ['comedy']
+  us_rating: 'PG-13'
+  year: 1975
   }
 ]
 
+first_user = User.first
+
+list.each do |attribute|
+first_user.list.create attribute
+end
