cf13662dbf0f805409b6b2e77cc28611a763e5b6
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-11-22 13:46:42 -0800

    schema and users updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,50 @@
-movies = [
-  {
-  title: '456 Street',
-  show_type: 'movie',
-  network: ['hulu']
-  genre: ['horror','foreign']
-  us_rating: 'R'
-  year: 2021
-  },
-  {
-  title: 'Jumanji',
-  show_type: 'movie',
-  network: ['Netflix', 'Disney']
-  genre: ['Adventure', 'comedy']
-  us_rating: 'PG-13'
-  year: 2019
-  },
-  {
-  title: '5th Element',
-  show_type: 'movie',
-  network: ['netflix']
-  genre: ['sci-fi','fantasy']
-  us_rating: 'PG-13'
-  year: 1997
-  },
-  {
-  title: 'Jeffersons',
-  show_type: 'tv show',
-  network: ['hulu']
-  genre: ['comedy']
-  us_rating: 'PG-13'
-  year: 1975
-  }
-]
+Movie.destroy_all
+MovieList.destroy_all
+List.destroy_all
 
 first_user = User.first
 
-list.each do |attribute|
-first_user.list.create attribute
-end
+first_list = List.create(list_name: "first list", user_id: first_user.id)
+
+# movies = [
+#   {
+#   title: '456 Street',
+#   show_type: 'movie',
+#   network: ['hulu'],
+#   genre: ['Horror','Foreign'],
+#   us_rating: 'R',
+#   year: 2021,
+#   },
+#   {
+#   title: 'Jumanji',
+#   show_type: 'movie',
+#   network: ['Netflix', 'Disney'],
+#   genre: ['Adventure', 'Comedy'],
+#   us_rating: 'PG-13',
+#   year: 2019,
+#   },
+#   {
+#   title: '5th Element',
+#   show_type: 'movie',
+#   network: ['netflix'],
+#   genre: ['Sci-fi','Fantasy'],
+#   us_rating: 'PG-13',
+#   year: 1997,
+#   },
+#   {
+#   title: 'Jeffersons',
+#   show_type: 'tv show',
+#   network: ['hulu'],
+#   genre: ['Comedy'],
+#   us_rating: 'PG-13',
+#   year: 1975
+#   }
+# ]
+
+
+
+# MovieList.create(list: horror, movie: movies[0])
+# MovieList.create(list: foreign, movie: movies.first)
+
+
+
