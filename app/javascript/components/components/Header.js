a24d5ff19617b376df791c85fce98c95020c0698
SunkissedQueen <baxtercm@outlook.com>
2021-11-23 11:20:52 -0800

    styled the header with color and alignment and place DS.store trakcing in git-ignore

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -14,27 +14,42 @@ import logo  from '../assets/logo.png'
           console.log(currentUser)
         return (
             <header>
+                <div className="nav-bar">
                 <NavLink to="/">
                     <img src={logo} alt="where itz app logo" className="logo" width="100px" height="100px"/>
                 </NavLink>
-                <div className="nav-bar">
-                    <ul>
+        
                         {!loggedIn &&
                         <>
-                            <a href={signInRoute} className="nav-link">Sign In</a>
+                            <ul>
+                                <a href={signInRoute} className="nav-link">Sign In</a>
+                                </ul>
+                                <ul>
                             <a href={newUserRoute} className="nav-link">Sign Up</a>
+                            </ul>
+                            <ul>
                             <NavLink to="/" className="nav-link">Search</NavLink>
+                            </ul>
                         </>
+
                         }
                         {loggedIn &&
                         <>
-                            <a href={signOutRoute} className="nav-link">Sign Out</a>
+                        <ul>
                             <h1>Hello, {currentUser.username}</h1>
+                            {/* </ul>
+                            <ul> */}
+                            <a href={signOutRoute} className="nav-link">Sign Out</a>
+                            {/* </ul>
+                            <ul> */}
                             <NavLink to="/mylists" className="nav-link">My Lists</NavLink>
+                            {/* </ul>
+                            <ul> */}
                             <NavLink to="/" className="nav-link">Search</NavLink>
+                            </ul>
                         </>
                         }
-                    </ul>
+                   
                 </div>
             </header>
         )
