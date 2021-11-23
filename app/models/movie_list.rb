1713b2fe809d43ee7583aa5272afd78ca32f11ee
Anthonyc4848 <anthonycastro4848@gmail.com>
2021-11-22 16:08:04 -0800

    stylesheets removed, rubocop ran - no changes, spaces changes to 2 instead of 4

--- app/models/movie_list.rb
+++ app/models/movie_list.rb
@@ -1,4 +1,4 @@
 class MovieList < ApplicationRecord
-    belongs_to :movie
-    belongs_to :list
+  belongs_to :movie
+  belongs_to :list
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,13 @@
 Movie.destroy_all
 MovieList.destroy_all
 List.destroy_all
-User.create username: "Anthony", email: "User@hotmail.com", password: "123456"
+User.create username: 'Anthony', email: 'User@hotmail.com', password: '123456'
 first_user = User.first
 
-first_list = List.create(list_name: "first list", user_id: first_user.id)
+first_list = List.create(list_name: 'first list', user_id: first_user.id)
+
 first_movie = Movie.create(title: '456 Street', show_type: 'movie', network: ['hulu'], genre: ['Horror','Foreign'], us_rating: 'R', year: 2021)
-movies = [
-  {
-  title: '456 Street',
-  show_type: 'movie',
-  network: ['hulu'],
-  genre: ['Horror','Foreign'],
-  us_rating: 'R',
-  year: 2021,
-  },
-  {
-  title: 'Jumanji',
-  show_type: 'movie',
-  network: ['Netflix', 'Disney'],
-  genre: ['Adventure', 'Comedy'],
-  us_rating: 'PG-13',
-  year: 2019,
-  },
-  {
-  title: '5th Element',
-  show_type: 'movie',
-  network: ['netflix'],
-  genre: ['Sci-fi','Fantasy'],
-  us_rating: 'PG-13',
-  year: 1997,
-  },
-  {
-  title: 'Jeffersons',
-  show_type: 'tv show',
-  network: ['hulu'],
-  genre: ['Comedy'],
-  us_rating: 'PG-13',
-  year: 1975
-  }
-]
 
-
+second_movie = Movie.create(title: 'Jumanji', show_type: 'movie', network: ['Netflix', 'Disney'], genre: ['Adventure', 'Comedy'], us_rating: 'PG-13', year: 2019)
 
 MovieList.create(list: first_list, movie: first_movie)
-
-
-
