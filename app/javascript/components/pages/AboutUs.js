f4f3380a71daa36f3de9d2c9a9a12144ec5c5b21
SunkissedQueen <baxtercm@outlook.com>
2021-11-26 13:26:25 -0800

    added more pics to assets, updated gif

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
 import teamcodecrak from '../assets/teamcodecrak.gif'
+import codeCrak from '../assets/codeCrak.gif'
+import batCrak from '../assets/batCrak.jpeg'
 
 class AboutUs extends Component {
-    render() {
-        return (
-					<div class="row">
-  <div class="column">
-    <div class="card">..</div>
-  </div>
-  <div class="column">
-    <div class="card">..</div>
-  </div>
-  <div class="column">
-    <div class="card">..</div>
-  </div>
-  <div class="column">
-    <div class="card">..</div>
-  </div>
-</div>
-
-        )
-    }
+  render() {
+    return (
+      <div>
+        <img src={codeCrak} alt='codecrak' width="200px"/>
+      
+			<div class="row">
+        <div class="column">
+          <div class="card">
+          <img src={batCrak} alt='codecrak' width="200px"/>
+          <div class="container">
+            <h4><b>Kelly Evans</b></h4>
+            <p>I am a full-stack software developer, transitioning from a career in federal service in the intelligence/investigations sector. After six rewarding years in federal service, and taking time to be a stay-at-home mom, I am excited to bring my skills into the web development industry. My background in creating collaborative solutions to dynamic and challenging problem-sets has lent itself well to the critical thinking and strong communication skills required in a pair programming environment.</p>
+          </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">..</div>
+        </div>
+        <div class="column">
+          <div class="card">..</div>
+        </div>
+        <div class="column">
+          <div class="card">..</div>
+        </div>
+      </div>
+      </div>
+    )
+  }
 }
 export default AboutUs
