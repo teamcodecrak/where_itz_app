5f731a0c0ceaa45e6d5fd4a53c30ac150d711f78
SunkissedQueen <baxtercm@outlook.com>
2021-12-02 00:15:22 -0800

    updated Readme and fix errors on the external link for official site

--- app/javascript/components/pages/ListItems.js
+++ app/javascript/components/pages/ListItems.js
@@ -11,19 +11,26 @@ class ListItems extends Component {
             <br />
             <Col sm="8" >
                 {movies.map(movie => {
-                    return(
-                        <Card body key={movie.id}>
+                    return (
+                      <Card body key={movie.id}>
                         <CardTitle>
-												<h4>Name: {movie.name}</h4>
-												<h4>Show Type: {movie.show_type}</h4>
-												<h4><a href="{movie.officialSite}" target="_blank">Visit the official site.</a></h4>
-												<h4>Network: {movie.network}</h4>
-												<img src={movie.image} alt="tv show poster" width="200px"/>
-												<h4>Summary: {movie.summary}</h4>
-
+                          <h4>Name: {movie.name}</h4>
+                          <h4>Show Type: {movie.show_type}</h4>
+                          <h4>
+                            <a href={movie.officialSite} target="_blank">
+                              Visit the official site.
+                            </a>
+                          </h4>
+                          <h4>Network: {movie.network}</h4>
+                          <img
+                            src={movie.image}
+                            alt="tv show poster"
+                            width="200px"
+                          />
+                          <h4>Summary: {movie.summary}</h4>
                         </CardTitle>
-                        </Card>
-                    )
+                      </Card>
+                    );
                 })}
             </Col>
         </div>
